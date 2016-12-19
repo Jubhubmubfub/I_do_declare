95542970b126a7decbe123648a9ba1429281a80b
Marcus Sakoda <Jubhub@yahool.com>
2016-12-19 12:16:47 -0800

    initial commit

---
+++ declare.js
@@ -0,0 +1,25 @@
+var num1 = 5;
+var num2 = 2.7;
+var num3 = -9;
+var num4 = 0.00001;
+
+var str1 = "Coding";
+var str2 = " Dojo";
+var str3 = " is awesome";
+
+var bool1 = true;
+var bool2 = false;
+
+var undef;
+
+console.log("The first number is: " + num1);
+console.log("The second number is: " + num2);
+console.log("The third number is: " + num3);
+console.log("The fourth number is: " + num4);
+
+console.log(str1 + str2 + str3);
+
+console.log("Boolean 1 is " + bool1);
+console.log("Boolean 2 is " + bool2);
+
+console.log("This variable is undefined: " undef);
